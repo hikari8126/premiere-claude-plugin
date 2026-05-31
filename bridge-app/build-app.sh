f366bb2b43f2a2f308a9d6da273d865d209dfbee
hikari8126 <hoang.vietnguyen@crossian.com>
2026-05-31 18:37:54 +0700

    chore: bump bridge app 2.12 → 2.13 (fix restart + show version)

--- bridge-app/build-app.sh
+++ bridge-app/build-app.sh
@@ -73,9 +73,9 @@ cat > "${APP_DIR}/Contents/Info.plist" << PLIST
   <key>CFBundleExecutable</key>
     <string>Claude Bridge</string>
   <key>CFBundleVersion</key>
-    <string>2.12</string>
+    <string>2.13</string>
   <key>CFBundleShortVersionString</key>
-    <string>2.12</string>
+    <string>2.13</string>
   <key>PluginVersion</key>
     <string>${PLUGIN_VERSION}</string>
   <key>CFBundlePackageType</key>
